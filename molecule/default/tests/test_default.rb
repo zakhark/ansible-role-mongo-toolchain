8b9a0e83aa337be8fea6ac0cd9ea77908ed6fab8
John Chen <johnchen456@gmail.com>
2019-10-17 16:50:56 -0400

    test: added test to test python 3.7 in toolchain

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+describe command('/opt/mongodbtoolchain/v3/bin/python3.7 --version') do
+  its('stdout') { should eq "Python 3.7.0\n" }
+  its('exit_status') { should eq 0 }
+end
