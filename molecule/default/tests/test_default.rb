5b673323e3cf53e05e238874333626e8d03c27a2
John Chen <johnchen456@gmail.com>
2019-10-18 16:48:20 -0400

    test: disable line length for test

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -19,7 +19,11 @@ describe file('/opt/mongodbtoolchain/v3/bin/gcc') do
   it { should exist }
 end
 
+# rubocop:disable LineLength
+
 describe command('/opt/mongodbtoolchain/v3/bin/gcc --version') do
   its('stderr') { should eq "gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516\nCopyright (C) 2016 Free Software Foundation, Inc.\nThis is free software; see the source for copying conditions.  There is NO\nwarranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n" }
   its('exit_status') { should eq 0 }
 end
+
+# rubocop:enable LineLength
