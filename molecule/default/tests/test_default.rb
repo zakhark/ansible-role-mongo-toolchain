202eec6664d25bb9e8fd0a48d30a3e8024d7f1a7
John Chen <johnchen456@gmail.com>
2019-10-18 16:02:12 -0400

    test: checking stderr

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-describe command('/opt/mongodbtoolchain/v3/bin/python3.7 --version') do
-  its('stdout') { should eq "Python 3.7.0\n" }
-  its('exit_status') { should eq 0 }
-end
-
 describe file('/opt/mongodbtoolchain') do
   its('type') { should eq :directory }
   it { should be_directory }
@@ -18,4 +13,14 @@ end
 describe file('/opt/mongodbtoolchain/v3/bin') do
   its('type') { should eq :directory }
   it { should be_directory }
+end
+
+describe file('/opt/mongodbtoolchain/v3/bin/python3.7') do
+  it { should exist }
+end
+
+describe command('/opt/mongodbtoolchain/v3/bin/python3.7 --version') do
+  its('stdout') { should eq "Python 3.7.0\n" }
+  its('stderr') { should eq "Python 3.7.0\n" }
+  its('exit_status') { should eq 0 }
 end
