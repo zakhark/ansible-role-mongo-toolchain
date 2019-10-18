29437c2a301c69826f7789e7c60323b18d09f287
John Chen <johnchen456@gmail.com>
2019-10-18 15:08:19 -0400

    test: added more tests for toolchain dir

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -4,3 +4,13 @@ describe command('/opt/mongodbtoolchain/v3/bin/python3.7 --version') do
   its('stdout') { should eq "Python 3.7.0\n" }
   its('exit_status') { should eq 0 }
 end
+
+describe file('/opt/mongodbtoolchain') do
+  its('type') { should eq :directory }
+  it { should be_directory }
+end
+
+describe file('/opt/mongodbtoolchain/v3') do
+  its('type') { should eq :directory }
+  it { should be_directory }
+end
