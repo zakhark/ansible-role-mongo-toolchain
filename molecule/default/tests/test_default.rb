9038654b8cb369d3c1a5c3635ae8bfb39c44133b
John Chen <johnchen456@gmail.com>
2019-10-24 11:58:26 -0400

    test: Adjsuted tests to use fake toolchain

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 
-describe file('/opt/mongodbtoolchain') do
+# rubocop:disable LineLength
+
+describe file('/opt/mongodbtoolchain/revisions/test_mongodbtoolchain/v3/bin') do
   its('type') { should eq :directory }
   it { should be_directory }
 end
 
-describe file('/opt/mongodbtoolchain/v3') do
-  its('type') { should eq :directory }
-  it { should be_directory }
-end
-
-describe file('/opt/mongodbtoolchain/v3/bin') do
-  its('type') { should eq :directory }
-  it { should be_directory }
-end
-
-describe file('/opt/mongodbtoolchain/v3/bin/gcc') do
+describe file('/opt/mongodbtoolchain/revisions/test_mongodbtoolchain/v3/bin/python3.7') do
   it { should exist }
 end
 
-# rubocop:disable LineLength
-
-describe command('/opt/mongodbtoolchain/v3/bin/gcc --version') do
-  its('stdout') { should eq "gcc (GCC) 8.2.0\nCopyright (C) 2018 Free Software Foundation, Inc.\nThis is free software; see the source for copying conditions.  There is NO\nwarranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n" }
+describe command('/opt/mongodbtoolchain/revisions/test_mongodbtoolchain/v3/bin/python3.7 --version') do
+  its('stdout') { should eq "Python 3.7.0\n" }
   its('exit_status') { should eq 0 }
 end
 
