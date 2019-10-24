b13bf46480b5c784fe809ece1ee290fe437918fb
John Chen <johnchen456@gmail.com>
2019-10-24 15:25:23 -0400

    test: Added tests to run symlinked files

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -11,7 +11,21 @@ describe file('/opt/mongodbtoolchain/revisions/test_mongodbtoolchain/v3/bin/pyth
   it { should exist }
 end
 
+describe file('/opt/mongodbtoolchain/v3/bin') do
+  its('type') { should eq :directory }
+  it { should be_directory }
+end
+
+describe file('/opt/mongodbtoolchain/v3/bin/python3.7') do
+  it { should exist }
+end
+
 describe command('/opt/mongodbtoolchain/revisions/test_mongodbtoolchain/v3/bin/python3.7 --version') do
+  its('stdout') { should eq "Python 3.7.0\n" }
+  its('exit_status') { should eq 0 }
+end
+
+describe command('/opt/mongodbtoolchain/v3/bin/python3.7 --version') do
   its('stdout') { should eq "Python 3.7.0\n" }
   its('exit_status') { should eq 0 }
 end
